d7066655c3c27308ec104a96feaef4a616169e56
cornelacm-ctrl <cornelacm@gmail.com>
2026-06-08 21:47:22 +0200

    Add files via upload

--- locations.js
+++ locations.js
@@ -6,15 +6,53 @@ const locations = [
     lng: 28.164855
   },
   {
-    name: "Mechanical Building",
+    name: "Building 5",
     type: "building",
-    lat: -25.7551,
-    lng: 28.2320
+    lat: -25.732144,
+    lng: 28.163585
   },
+
   {
-    name: "Parking A",
+    name: "Building 6 Parking Lot C",
     type: "parking",
-    lat: -25.7538,
-    lng: 28.2309
+    lat: -25.732704,
+    lng: 28.165733
   }
 ];
+
+/*L.marker([-25.732823, 28.164855])
+.addTo(map)
+.bindPopup("Building 6");
+
+L.marker([-25.732704, 28.165733])
+.addTo(map)
+.bindPopup("Building 6 Parking Lot 3");
+
+L.marker([-25.733536, 28.164270])
+.addTo(map)
+.bindPopup("Building 6 Parking Lot 1");
+
+L.marker([-25.733236, 28.164968])
+.addTo(map)
+.bindPopup("Building 6 Parking Lot 2");
+
+L.marker([-25.731770, 28.165387])
+.addTo(map)
+.bindPopup("Building 7 Theunis Bester Hall");
+
+L.marker([-25.731919, 28.165107])
+.addTo(map)
+.bindPopup("Building 7 Parking Lot");
+
+L.marker([-25.731957, 28.164450])
+.addTo(map)
+.bindPopup("Building 5 Parking Lot");
+
+L.marker([-25.732202, 28.164422])
+.addTo(map)
+.bindPopup("Building 5");
+
+L.marker([-25.732144, 28.163585])
+.addTo(map)
+.bindPopup("Building 3");*/
+
--- script.js
+++ script.js
@@ -16,7 +16,7 @@ navigator.geolocation.getCurrentPosition(
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
 
-        map.setView([lat,lng],18);
+        map.setView([-25.734012, 28.163396/*lat,lng*/],18);
 
         L.marker([lat,lng])
             .addTo(map)
@@ -24,41 +24,7 @@ navigator.geolocation.getCurrentPosition(
             .openPopup();
     }
 );
-L.marker([-25.732823, 28.164855])
-.addTo(map)
-.bindPopup("Building 6");
 
-L.marker([-25.732704, 28.165733])
-.addTo(map)
-.bindPopup("Building 6 Parking Lot 3");
-
-L.marker([-25.733536, 28.164270])
-.addTo(map)
-.bindPopup("Building 6 Parking Lot 1");
-
-L.marker([-25.733236, 28.164968])
-.addTo(map)
-.bindPopup("Building 6 Parking Lot 2");
-
-L.marker([-25.731770, 28.165387])
-.addTo(map)
-.bindPopup("Building 7 Theunis Bester Hall");
-
-L.marker([-25.731919, 28.165107])
-.addTo(map)
-.bindPopup("Building 7 Parking Lot");
-
-L.marker([-25.731957, 28.164450])
-.addTo(map)
-.bindPopup("Building 5 Parking Lot");
-
-L.marker([-25.732202, 28.164422])
-.addTo(map)
-.bindPopup("Building 5");
-
-L.marker([-25.732144, 28.163585])
-.addTo(map)
-.bindPopup("Building 3");
 
 locations.forEach(loc => {
   L.marker([loc.lat, loc.lng])
@@ -83,40 +49,3 @@ document.getElementById("searchBox").addEventListener("input", function (e) {
   }
 });
 
-let routeControl;
-
-function goTo(destination) {
-
-  navigator.geolocation.getCurrentPosition(pos => {
-
-    const userLat = pos.coords.latitude;
-    const userLng = pos.coords.longitude;
-
-    if (routeControl) {
-      map.removeControl(routeControl);
-    }
-
-    routeControl = L.Routing.control({
-      waypoints: [
-        L.latLng(userLat, userLng),
-        L.latLng(destination.lat, destination.lng)
-      ],
-      routeWhileDragging: false
-    }).addTo(map);
-
-  });
-}
-
-if (match) {
-  map.setView([match.lat, match.lng], 18);
-
-  L.popup()
-    .setLatLng([match.lat, match.lng])
-    .setContent(`
-      <b>${match.name}</b><br>
-      <button onclick="goTo(${JSON.stringify(match)})">
-        Get Directions
-      </button>
-    `)
-    .openOn(map);
-}
